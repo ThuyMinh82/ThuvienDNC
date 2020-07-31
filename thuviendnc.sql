1875f6e253cfc36f237a14e633da2080d3103da8
Đỗ Minh <dongothuyminh@gmail.com>
2020-07-31 15:54:10 +0700

    Update thuviendnc.sql

--- thuviendnc.sql
+++ thuviendnc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 31, 2020 lúc 09:05 AM
+-- Thời gian đã tạo: Th7 31, 2020 lúc 10:52 AM
 -- Phiên bản máy phục vụ: 10.4.13-MariaDB
 -- Phiên bản PHP: 7.2.31
 
@@ -37,7 +37,7 @@ CREATE TABLE `baiviet` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `status` int(11) NOT NULL,
   `cd_id` int(10) UNSIGNED NOT NULL,
-  `users_id` int(10) UNSIGNED NOT NULL
+  `user_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -50,34 +50,8 @@ CREATE TABLE `chudebv` (
   `id` int(10) UNSIGNED NOT NULL,
   `ten_cd` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `alias` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `bv_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `chudebv`
---
-
-INSERT INTO `chudebv` (`id`, `ten_cd`, `alias`, `bv_id`, `created_at`, `updated_at`) VALUES
-(1, 'Góc tản mạn sinh viên', 'goc-tan-man-sinh-vien', 1, NULL, NULL),
-(2, 'Tin tức & sự kiện', 'tin-tuc-&-su-kien', 2, NULL, NULL),
-(4, 'Giới thiệu sách', 'gioi-thieu-sach', 3, NULL, NULL),
-(5, 'Sách tặng', 'sach-tang', 4, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -89,7 +63,9 @@ CREATE TABLE `failed_jobs` (
 CREATE TABLE `hinhanh` (
   `id` int(10) UNSIGNED NOT NULL,
   `link_anh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `bv_id` int(10) UNSIGNED NOT NULL
+  `bv_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -109,14 +85,15 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(22, '2014_10_12_000000_create_users_table', 1),
-(23, '2019_08_19_000000_create_failed_jobs_table', 1),
-(24, '2020_07_31_031556_create_baiviet_table', 1),
-(25, '2020_07_31_034713_create_chudebv_table', 1),
-(26, '2020_07_31_035114_create_hinhanh_table', 1),
-(27, '2020_07_31_062901_create_phanquyen_table', 1),
-(28, '2020_07_31_063013_create_profileuser_table', 1),
-(29, '2020_07_31_063034_create_usergroup_table', 1);
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2020_07_31_082819_create_chudebv_table', 1),
+(5, '2020_07_31_084203_create_usergroup_table', 2),
+(6, '2020_07_31_084219_create_chudebv_table', 2),
+(7, '2020_07_31_084240_create_profileuser_table', 2),
+(8, '2020_07_31_084257_create_phanquyen_table', 2),
+(9, '2020_07_31_084319_create_users_table', 2),
+(10, '2020_07_31_084336_create_baiviet_table', 2),
+(11, '2020_07_31_084354_create_hinhanh_table', 2);
 
 -- --------------------------------------------------------
 
@@ -126,7 +103,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `phanquyen` (
   `id` int(10) UNSIGNED NOT NULL,
-  `ten_pg` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ten_pq` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `usergroup_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -144,7 +121,6 @@ CREATE TABLE `profileuser` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `sdt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `users_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -158,8 +134,6 @@ CREATE TABLE `profileuser` (
 CREATE TABLE `usergroup` (
   `id` int(10) UNSIGNED NOT NULL,
   `name_group` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `users_id` int(10) UNSIGNED NOT NULL,
-  `phanquyen_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -190,29 +164,23 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `baiviet`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `baiviet_id_unique` (`id`),
-  ADD UNIQUE KEY `baiviet_ten_bv_unique` (`ten_bv`);
+  ADD UNIQUE KEY `baiviet_ten_bv_unique` (`ten_bv`),
+  ADD KEY `baiviet_cd_id_foreign` (`cd_id`),
+  ADD KEY `baiviet_user_id_foreign` (`user_id`);
 
 --
 -- Chỉ mục cho bảng `chudebv`
 --
 ALTER TABLE `chudebv`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `chudebv_id_unique` (`id`),
   ADD UNIQUE KEY `chudebv_ten_cd_unique` (`ten_cd`);
-
---
--- Chỉ mục cho bảng `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `hinhanh`
 --
 ALTER TABLE `hinhanh`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `hinhanh_id_unique` (`id`);
+  ADD KEY `hinhanh_bv_id_foreign` (`bv_id`);
 
 --
 -- Chỉ mục cho bảng `migrations`
@@ -225,30 +193,28 @@ ALTER TABLE `migrations`
 --
 ALTER TABLE `phanquyen`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `phanquyen_id_unique` (`id`);
+  ADD KEY `phanquyen_usergroup_id_foreign` (`usergroup_id`);
 
 --
 -- Chỉ mục cho bảng `profileuser`
 --
 ALTER TABLE `profileuser`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `profileuser_id_unique` (`id`),
   ADD UNIQUE KEY `profileuser_email_unique` (`email`);
 
 --
 -- Chỉ mục cho bảng `usergroup`
 --
 ALTER TABLE `usergroup`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `usergroup_id_unique` (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_id_unique` (`id`),
-  ADD UNIQUE KEY `users_username_unique` (`username`);
+  ADD KEY `users_usergroup_id_foreign` (`usergroup_id`),
+  ADD KEY `users_profileuser_id_foreign` (`profileuser_id`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
@@ -264,13 +230,7 @@ ALTER TABLE `baiviet`
 -- AUTO_INCREMENT cho bảng `chudebv`
 --
 ALTER TABLE `chudebv`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT cho bảng `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `hinhanh`
@@ -282,7 +242,7 @@ ALTER TABLE `hinhanh`
 -- AUTO_INCREMENT cho bảng `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT cho bảng `phanquyen`
@@ -307,6 +267,36 @@ ALTER TABLE `usergroup`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `baiviet`
+--
+ALTER TABLE `baiviet`
+  ADD CONSTRAINT `baiviet_cd_id_foreign` FOREIGN KEY (`cd_id`) REFERENCES `chudebv` (`id`),
+  ADD CONSTRAINT `baiviet_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Các ràng buộc cho bảng `hinhanh`
+--
+ALTER TABLE `hinhanh`
+  ADD CONSTRAINT `hinhanh_bv_id_foreign` FOREIGN KEY (`bv_id`) REFERENCES `baiviet` (`id`);
+
+--
+-- Các ràng buộc cho bảng `phanquyen`
+--
+ALTER TABLE `phanquyen`
+  ADD CONSTRAINT `phanquyen_usergroup_id_foreign` FOREIGN KEY (`usergroup_id`) REFERENCES `usergroup` (`id`);
+
+--
+-- Các ràng buộc cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_profileuser_id_foreign` FOREIGN KEY (`profileuser_id`) REFERENCES `profileuser` (`id`),
+  ADD CONSTRAINT `users_usergroup_id_foreign` FOREIGN KEY (`usergroup_id`) REFERENCES `usergroup` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
