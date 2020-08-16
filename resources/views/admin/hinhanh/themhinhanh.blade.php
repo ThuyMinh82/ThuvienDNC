de1e6d583ddc6ce282d09fa431886c81d48f397c
jasonnguyen0204 <jasonnguyen0204@users.noreply.github.com>
2020-08-16 19:06:27 +0700

    .

--- resources/views/admin/hinhanh/themhinhanh.blade.php
+++ resources/views/admin/hinhanh/themhinhanh.blade.php
@@ -8,37 +8,77 @@
 <div class="row no-gutters">
     <div class="col-md-3"></div>
     <div class="col-md-6 col-sm-12 padding-0">
-        <div class="alert alert-success alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <strong>Đăng nhập thành công!</strong> Bạn có thể thay đổi thông tin cá nhân và sử dụng hệ thống.
+			<!--/.col-lg-12-->
+        <div style="padding-bottom: 120px">
+	        @if(count($errors) > 0)
+	        	<div class="alert alert-danger">
+	        		@foreach($errors->all() as $err)
+	        			{{$err}}<br>
+	        		@endforeach
+	        	</div>
+	        @endif
+
+	        @if(session('thongbao'))
+                <div class="alert alert-success">
+                	{{session('thongbao')}}
+                </div>
+            @endif
+			<h1 style="text-align:center;">Thêm Hình Ảnh</h1>
+			<form action="admin/chudebv/themchude" method="POST">
+			    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+			    <div class="from-group">
+                    <table class="table table-borderless">
+                        <tbody>
+                            <tr>
+                                <td colspan='2'>
+                                    <input class="form-control" name="ten_cd" placeholder="Nhập tên ảnh ..." />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style='width: 81%;'>
+                                    <input id="ckfinder-input-1" class="form-control" type="text">
+                                </td>
+                                <td>
+                                    <a class="btn btn-success" id="ckfinder-popup-1">Chọn ảnh</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <script type="text/javascript" src="{{asset('/ckfinder/ckfinder.js')}}"></script>
+                    <script>
+                        	var button1 = document.getElementById( 'ckfinder-popup-1' );
+
+                            button1.onclick = function() {
+                                selectFileWithCKFinder( 'ckfinder-input-1' );
+                            };
+
+                            function selectFileWithCKFinder( elementId ) {
+                                CKFinder.popup( {
+                                    chooseFiles: true,
+                                    width: 800,
+                                    height: 600,
+                                    onInit: function( finder ) {
+                                        finder.on( 'files:choose', function( evt ) {
+                                            var file = evt.data.files.first();
+                                            var output = document.getElementById( elementId );
+                                            output.value = file.getUrl();
+                                        } );
+
+                                        finder.on( 'file:choose:resizedImage', function( evt ) {
+                                            var output = document.getElementById( elementId );
+                                            output.value = evt.data.resizedUrl;
+                                        } );
+                                    }
+                                } );
+                            }
+                    </script>
+			    </div>
+			    <button type="submit" class="btn btn-primary center" style="margin-top: 30px;">Thêm</button>
+            </form>
         </div>
-        <h4>THÔNG TIN TÀI KHOẢN</h4>       
-        <table class="table table-hover">
-            <tbody>
-            <tr>
-                <td id='bold'>Họ Tên:</td>
-                <td>Nguyễn Hoàng Giang</td>
-            </tr>
-            <tr>
-                <td id='bold'>Email:</td>
-                <td>admin@example.com</td>
-            </tr>
-            <tr>
-                <td id='bold'>Địa chỉ:</td>
-                <td>2338/4, ấp Mỹ Hưng 1, xã Mỹ Hòa, thị xã Bình Minh, tỉnh Vĩnh Long</td>
-            </tr>
-            <tr>
-                <td id='bold'>Số điện thoại:</td>
-                <td>0789539650</td>
-            </tr>
-            </tbody>
-        </table>
     </div>
-    <div class="col-md-3"></div> 
+    <div class="col-md-3"></div>
+        
 </div>
-<button type="button" class="btn btn-primary center"><a href="changepro.html" id="a-none">Lưu</a></button>
-<br>
-<br>
-<br>
 <br>
 @endsection()
